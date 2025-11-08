3c334edc1343c24de61cc7ccc3e5b1e73a000b4f
minwoo1119 <minwoo1119@naver.com>
2025-11-08 23:30:09 +0900

    feat: selectBar 적용 #5

--- src/components/techs/Techs.tsx
+++ src/components/techs/Techs.tsx
@@ -1,9 +1,20 @@
+import { SelectTabBar } from '../common/selectTabBar/SelectTabBar';
 import styles from './techs.module.scss';
+import { useState } from 'react';
+
+export type TabBarOption = '프론트엔드' | '백엔드' | 'AI';
 
 export const Techs = () => {
+  const [selected, setSelected] = useState<TabBarOption>('프론트엔드');
   return (
     <div className={styles.container}>
-      <div>hii</div>
+      <SelectTabBar
+        op1='프론트엔드'
+        op2='백엔드'
+        op3='AI'
+        onSelect={(op: TabBarOption) => setSelected(op)}
+        selected={selected}
+      />
     </div>
   );
 };
