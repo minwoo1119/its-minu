b02d2aad3b783e415f4501f06490688415c475e8
minwoo1119 <minwoo1119@naver.com>
2025-11-09 22:14:12 +0900

    feat: 기술 페이지 UI 퍼블리싱 #5

--- src/components/techs/Techs.tsx
+++ src/components/techs/Techs.tsx
@@ -15,6 +15,29 @@ export const Techs = () => {
         onSelect={(op: TabBarOption) => setSelected(op)}
         selected={selected}
       />
+      <div className={styles.text}>
+        많이 부족하지만 열심히 깊이를 채워나가는 중이에요.
+      </div>
+      <div className={styles.techStacks}>
+        {selected === '프론트엔드' ? (
+          <img
+            src='src/assets/img/frontend_stack.jpg'
+            alt='프론트엔드 기술스택'
+          />
+        ) : (
+          ''
+        )}
+        {selected === '백엔드' ? (
+          <img src='src/assets/img/backend_stack.jpg' alt='백엔드 기술스택' />
+        ) : (
+          ''
+        )}
+        {selected === 'AI' ? (
+          <img src='src/assets/img/ai_stack.jpg' alt='AI 기술스택' />
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 };
