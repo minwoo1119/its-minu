7b04d48d57a3b6ed9f395bbe1b8e465a16f953ab
minwoo1119 <minwoo1119@naver.com>
2025-11-10 22:17:48 +0900

    feat: Project 페이지에 반영 #7

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,28 @@
+import { Modal } from '../common/modal/Modal';
 import { ProjectBox } from '../common/projectBox/ProjectBox';
-import { projectsData } from '../../../public/data/projectsData';
+import {
+  projectsData,
+  type ProjectInfo,
+} from '../../../public/data/projectsData';
 import styles from './projects.module.scss';
 import { useState } from 'react';
+import { DetailProjects } from '../detailProjects/DetailProjects';
 
 export const Projects = () => {
   const [showAll, setShowAll] = useState(false);
+  const [selectedProjectId, setSelectedProjectId] = useState<number | null>(
+    null
+  );
 
   const visibleProjects = showAll ? projectsData : projectsData.slice(0, 3);
+
+  const handleProjectClick = (project: ProjectInfo) => {
+    setSelectedProjectId(project.id);
+  };
+
+  const closeModal = () => {
+    setSelectedProjectId(null);
+  };
 
   return (
     <div className={styles.container}>
@@ -14,7 +30,7 @@ export const Projects = () => {
       <div className={styles.titleText}>프로젝트</div>
       <div className={styles.projects}>
         {visibleProjects.map((item) => (
-          <ProjectBox {...item} />
+          <ProjectBox {...item} onClick={() => handleProjectClick(item)} />
         ))}
       </div>
 
@@ -24,6 +40,15 @@ export const Projects = () => {
       >
         {showAll ? '접기' : '더보기'}
       </button>
+
+      {selectedProjectId && (
+        <>
+          <div className={styles.overlay} onClick={closeModal} />
+          <Modal onClose={closeModal}>
+            <DetailProjects projectId={selectedProjectId} />
+          </Modal>
+        </>
+      )}
     </div>
   );
 };
