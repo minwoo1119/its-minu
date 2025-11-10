f86aeac1d47302ddfda65a0e95872b23157d7aaf
minwoo1119 <minwoo1119@naver.com>
2025-11-10 22:16:42 +0900

    feat: 모달 컴포넌트 구현 #7

---
+++ src/components/common/modal/Modal.tsx
@@ -0,0 +1,20 @@
+import type { ReactElement } from 'react';
+import styles from './modal.module.scss';
+
+interface Props {
+  onClose: () => void;
+  children: ReactElement;
+}
+
+export const Modal = ({ onClose, children }: Props) => {
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.container} onClick={(e) => e.stopPropagation()}>
+        <button className={styles.closeBtn} onClick={onClose}>
+          ✕
+        </button>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
