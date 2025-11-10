7a2cae9aaad0f3bd02a2722275080f860d34fbfd
minwoo1119 <minwoo1119@naver.com>
2025-11-10 22:17:36 +0900

    fix: 불필요한 요소 제거 #7

--- src/components/common/projectBox/ProjectBox.tsx
+++ src/components/common/projectBox/ProjectBox.tsx
@@ -1,15 +1,19 @@
+import type { ProjectInfo } from '../../../../public/data/projectsData';
 import styles from './projectBox.module.scss';
 
-interface Props {
-  imgUrl: string;
-  title: string;
-  description: string;
-  techs: string[];
+interface Props extends ProjectInfo {
+  onClick: () => void;
 }
 
-export const ProjectBox = ({ imgUrl, title, description, techs }: Props) => {
+export const ProjectBox = ({
+  imgUrl,
+  title,
+  description,
+  techs,
+  onClick,
+}: Props) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={onClick}>
       <img src={imgUrl} alt={title} />
       <div className={styles.title}>{title}</div>
       <div className={styles.description}>{description}</div>
