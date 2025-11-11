475831ead25bfc5382768ed06712f85109ade98b
minwoo1119 <minwoo1119@naver.com>
2025-11-11 10:40:52 +0900

    fix: Header 클릭 링크 추가 #9

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,11 @@
 import styles from './header.module.scss';
 
-export const Header = () => {
+interface HeaderProps {
+  onTechClick: () => void;
+  onProjectClick: () => void;
+}
+
+export const Header = ({ onTechClick, onProjectClick }: HeaderProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.dots}>
@@ -9,9 +14,19 @@ export const Header = () => {
         <div className={styles.dot}></div>
       </div>
       <div className={styles.selectTab}>
-        <div className={styles.tab}>기술</div>
-        <div className={styles.tab}>프로젝트</div>
-        <div className={styles.tab}>블로그</div>
+        <div className={styles.tab} onClick={onTechClick}>
+          기술
+        </div>
+        <div className={styles.tab} onClick={onProjectClick}>
+          프로젝트
+        </div>
+        <a
+          className={styles.tab}
+          href='https://lmw1119.tistory.com/'
+          target='_blank'
+        >
+          블로그
+        </a>
       </div>
     </div>
   );
