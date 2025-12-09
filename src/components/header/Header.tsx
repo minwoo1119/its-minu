7dccf923c35a7d764845e6cee5cdb3dd08f4da76
minwoo1119 <minwoo1119@naver.com>
2025-12-09 10:34:12 +0900

    feat: Header 컴포넌트 반응형 적용 #11

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import styles from './header.module.scss';
+import { useState } from 'react';
 
 interface HeaderProps {
   onTechClick: () => void;
@@ -6,6 +7,13 @@ interface HeaderProps {
 }
 
 export const Header = ({ onTechClick, onProjectClick }: HeaderProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClick = (callback?: () => void) => {
+    if (callback) callback();
+    setIsOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.dots}>
@@ -13,17 +21,34 @@ export const Header = ({ onTechClick, onProjectClick }: HeaderProps) => {
         <div className={styles.dot}></div>
         <div className={styles.dot}></div>
       </div>
-      <div className={styles.selectTab}>
-        <div className={styles.tab} onClick={onTechClick}>
+
+      <button
+        className={styles.hamburger}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label='Menu Toggle'
+      >
+        {isOpen ? '✕' : '☰'}
+      </button>
+
+      <div className={`${styles.selectTab} ${isOpen ? styles.open : ''}`}>
+        <div
+          className={styles.tab}
+          onClick={() => handleMenuClick(onTechClick)}
+        >
           기술
         </div>
-        <div className={styles.tab} onClick={onProjectClick}>
+        <div
+          className={styles.tab}
+          onClick={() => handleMenuClick(onProjectClick)}
+        >
           프로젝트
         </div>
         <a
           className={styles.tab}
           href='https://lmw1119.tistory.com/'
           target='_blank'
+          rel='noreferrer'
+          onClick={() => setIsOpen(false)}
         >
           블로그
         </a>
